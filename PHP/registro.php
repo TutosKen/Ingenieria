f361dc826356bab94d3d5aad1651fe9746bc765c
TutosKen <tutos1620@gmail.com>
2020-09-26 18:07:22 -0400

    Inicio de formulario registro

--- PHP/registro.php
+++ PHP/registro.php
@@ -6,24 +6,25 @@ session_start();
 <html lang="en">
 <head>
     <?php 
-        require_once('PHP/head.php');
+        require_once('head.php');
     ?>
     <title>Animales</title>
 </head>
 <body>
 
-<div class="card mx-auto" style="width: 18rem;">
-<img class="card-img-top" src="img/logo.jpg" alt="Card image cap">
+<div class="card mx-auto" style="width: 30rem;">
+<img class="card-img-top" src="../img/logo.jpg" alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title text-center">Animales</h5>
 
-    <form action="PHP/login-action.php" method="POST">
-        <div class="form-group" id="email-input">
-            <label class="fuente-custom" for="Email">Email</label>
-            <input type="text" name="email" class="form-control fuente-custom" id="Email" placeholder="Correo electronico">
+    <form action="registro-action.php" method="POST">
+        <div class="form-group" id="info-registro">
+            <p>Informacion general</p>
+            <input type="text" name="email" class="form-control fuente-custom" id="Nombre" placeholder="Nombre">
+            <input type="text" name="email" class="form-control fuente-custom" id="Apellido" placeholder="Apellido">
         </div>
 
-        <div class="form-group" id="pass-input">
+        <div class="form-group" id="pass-registro">
             <label class="fuente-custom" for="Contrasenna">Contraseña</label>
             <input type="password" name="pass" class="form-control" id="Contrasenna" placeholder="Contraseña">
         </div>
@@ -44,5 +45,10 @@ session_start();
     </div>
 
 </div>
+
+    <?php
+        require_once('../JS.php');
+    
+    ?>
 </body>
 </html>
